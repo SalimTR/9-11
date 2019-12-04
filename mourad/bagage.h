6040e9efb500cf38a0cbf923bb2e06de324631cd
zr1b1 <48069991+zr1b1@users.noreply.github.com>
2019-12-04 19:58:12 +0100

    Add files via upload

---
+++ mourad/bagage.h
@@ -0,0 +1,32 @@
+#ifndef BAGAGE_H
+#define BAGAGE_H
+#include <QString>
+#include <QTableView>
+#include <QSqlQueryModel>
+#include <QTableView>
+
+class QMediaPlayer;
+
+class Bagage
+{
+    int idVoy ;
+    int code;
+    QString poids;
+public:
+    Bagage();
+    Bagage(int, int, QString);
+    bool ajouter();
+    QSqlQueryModel* afficher();
+    void supprimer(int code);
+    bool modifier();
+    QSqlQueryModel* trierpoids();
+    void clearTable(QTableView* table);
+    void searchRegexp(QTableView *table, QString x);
+    bool recherche(int code);
+    ~Bagage(){};
+private:
+    QMediaPlayer *player;
+    QMediaPlayer *player_button;
+};
+
+#endif // BAGAGE_H
